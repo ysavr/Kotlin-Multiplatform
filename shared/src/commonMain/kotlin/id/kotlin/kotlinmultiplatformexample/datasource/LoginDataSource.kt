4eb221403b338f82aa973dee72df83476e279f39
ysavr <y.safrudin666@gmail.com>
2021-05-31 15:54:45 +0700

    [Shared] add validation

--- shared/src/commonMain/kotlin/id/kotlin/kotlinmultiplatformexample/datasource/LoginDataSource.kt
+++ shared/src/commonMain/kotlin/id/kotlin/kotlinmultiplatformexample/datasource/LoginDataSource.kt
@@ -9,11 +9,17 @@ class LoginDataSource {
     @ExperimentalStdlibApi
     fun login(username: String, password: String): Result<UserModel> {
         return try {
-            if (password.lowercase() == username.lowercase()) {
-                val id = Random.nextInt(0, 2000)
-                val user = UserModel(id.toString(), username)
-                Result.Success(user)
-            } else Result.Error(RuntimeException("Password not matches with username"))
+            when {
+                password.isEmpty() and username.isEmpty() -> {
+                    Result.Error(RuntimeException("Field must not empty "))
+                }
+                password.lowercase() == username.lowercase() -> {
+                    val id = Random.nextInt(0, 2000)
+                    val user = UserModel(id.toString(), username)
+                    Result.Success(user)
+                }
+                else -> Result.Error(RuntimeException("Password not matches with username"))
+            }
         } catch (e: Throwable) {
             Result.Error(RuntimeException("Error login", e))
         }
